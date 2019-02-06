ccaa12dcdd847115267b2bf81f789db858dc3c59
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-06 01:12:14 +0000

    Got it!

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ sum(1, 3) = 1 + (1 + 1) + (1 + 1 + 1);
 */
 function sum(fromN, toN) {
   if (fromN === toN) {
-    return toN;
+    return toN; // end
   }
-
-  // return
-  // Sum all the values from fromN up to toN
+  return fromN + sum(fromN + 1, toN)
 }
 
 console.log(sum(3, 7));
