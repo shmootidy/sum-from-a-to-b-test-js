f8a5e60d762670596ed2b98d953f206ca0e20997
Shmoo Ritchie <shmooritchie@gmail.com>
2019-02-06 01:02:04 +0000

    Write notes working out recursion.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+// sum(1, 1) = 1;
+// sum(1, 2) = 2 + 1;
+// sum(1, 3) = 3 + 2 + 1;
 
 function sum(fromN, toN) {
+  if (fromN === toN) {
+    return toN;
+  }
+
+  // return
   // Sum all the values from fromN up to toN
 }
 
+console.log(sum(3, 7));
 module.exports = sum;
